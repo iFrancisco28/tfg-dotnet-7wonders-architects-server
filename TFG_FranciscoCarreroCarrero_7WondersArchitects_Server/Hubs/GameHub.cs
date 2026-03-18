30b4cb56e81d35f41ab2976b653c0468b1140b32
Francisco Carrero Carrero <fracarrero05@gmail.com>
2026-03-18 01:04:57 +0100

    -controlamos que no escojan misma maravilla

--- TFG_FranciscoCarreroCarrero_7WondersArchitects_Server/Hubs/GameHub.cs
+++ TFG_FranciscoCarreroCarrero_7WondersArchitects_Server/Hubs/GameHub.cs
@@ -2,17 +2,22 @@
 using System.Collections.Concurrent;
 
 namespace TFG_FranciscoCarreroCarrero_7WondersArchitects_Server.Hubs {
+    public class RoomInfo {
+        public string HostName { get; set; }
+        public string HostWonder { get; set; }
+    }
     public class GameHub : Hub {
         //un diccionario al que pueden acceder desde distintos hilos a la vez
-        private static readonly ConcurrentDictionary<string, bool> SalasActivas = new();
+        private static readonly ConcurrentDictionary<string, RoomInfo> SalasActivas = new();
 
         //creamos sala devolvemos codigo
-        public async Task<string> CreateRoom() {
+        public async Task<string> CreateRoom(string hostName, string hostWonder) {
             //creo el codigo de sala (le quito los guiones, cojo 4 y a mayusculas)
             string code = Guid.NewGuid().ToString("N").Substring(0, 4).ToUpper();
 
             //registramos sala
-            SalasActivas.TryAdd(code, true);
+            var roomInfo = new RoomInfo { HostName = hostName, HostWonder = hostWonder };
+            SalasActivas.TryAdd(code, roomInfo);
 
             //metemos al creador en el grupo de signalR
             await Groups.AddToGroupAsync(Context.ConnectionId, code);
@@ -21,20 +26,26 @@ namespace TFG_FranciscoCarreroCarrero_7WondersArchitects_Server.Hubs {
         }
 
         //intentamos unirnos, false si no existe la sala pasada por args
-        public async Task<bool> JoinRoom(string roomCode) {
+        public async Task<string> JoinRoom(string roomCode, string guestName, string guestWonder) {
             string code = roomCode.ToUpper();
             
-            if (SalasActivas.ContainsKey(code)) {
+            if (SalasActivas.TryGetValue(code, out RoomInfo room)) {
+
+                //si la maravilla esta repetida
+                if (room.HostWonder == guestWonder) {
+                    return "WONDER_TAKEN";
+                }
+
                 //si el codigo existe unimos al usuario al grupo
                 await Groups.AddToGroupAsync(Context.ConnectionId, code);
 
                 //y se avisa
-                await Clients.Group(code).SendAsync("ReceiveMessage", "Sistema", "Un nuevo jugador se ha unido a la sala");
+                await Clients.Group(code).SendAsync("ReceiveMessage", "Sistema", $"{guestName} se ha unido a la sala");
 
-                return true;
+                return "OK";
             }
 
-            return false; //sala no encontrada, el jugador no se une
+            return "NOT_FOUND"; //sala no encontrada, el jugador no se une
         }
 
         // metodo generico
